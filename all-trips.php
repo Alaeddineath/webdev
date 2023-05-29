4b66da0b82eda04b1fdcdffc1974886d24ee2358
Alaeddineath <alaeddine.atamna@ensia.edu.dz>
2023-05-29 15:37:13 +0100

    last one

--- all-trips.php
+++ all-trips.php
@@ -5,7 +5,7 @@
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-  <title>RIHLAT-e Bootstrap Template - Index</title>
+  <title>RIHLAT-e </title>
   <meta content="" name="description">
   <meta content="" name="keywords">
 <meta name="viewport" content="width=device-width, initial-scale=1,0">
@@ -25,7 +25,7 @@
   <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
 
   <!-- Template Main CSS File -->
-  <link href="assets/css/style.css" rel="stylesheet">
+  <link href="assets/css/all-trips.css" rel="stylesheet">
 
   <!-- =======================================================
   * Template Name: Squadfree - v4.11.0
@@ -34,10 +34,17 @@
   * License: https://bootstrapmade.com/license/
   ======================================================== -->
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+    
 <style>
 .checked {
   color: orange;
 }
+.pagination-wrapper {
+            display: flex;
+            justify-content: center;
+            margin-top: 20px;
+        }
 </style>
 </head>
 
@@ -48,7 +55,7 @@
     <div class="container d-flex align-items-center justify-content-between position-relative">
 
       <div class="logo">
-        <h1 class="text-light"><a href="index.html"><span>RIHLAT-e</span></a></h1>
+        <h1 class="text-light"><a href="loggedindex.php"><span>RIHLAT-e</span></a></h1>
         <!-- Uncomment below if you prefer to use an image logo -->
         <!-- <a href="index.html"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
       </div>
@@ -71,69 +78,99 @@
 
 
   <main id="main">
-  
+
     <!-- ======= Portfolio Section ======= -->
     <section id="portfolio" class="portfolio" style="background-color:rgb(249, 249, 250);">
-      <h1 class="sec__title">Browse our trips !</h1>
-      <div class="gallery1" >
-      <?php 
-  $dbhost = 'localhost';
-  $dbuser = 'root';
-  $dbpass = '';
-  $dbname = 'rihlate';
-  
-  $conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
+        <h1 class="sec__title">Browse our trips!</h1>
+        <div class="gallery1">
+            <?php
+            $dbhost = 'localhost';
+            $dbuser = 'root';
+            $dbpass = '';
+            $dbname = 'rihlate';
 
-  $query = "SELECT* FROM travel_plan";
-  
+            $conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
 
-  if ($result=mysqli_query($conn,$query))
-  {
-  // Fetch one and one row
-  while ($row=mysqli_fetch_row($result))
-  {
-  $travel_id = $row[0];
-  $travel_name = $row[1];                          
-  $travel_date = $row[2];
-  $travel_price =  $row[3];
-  $travel_duration = $row[4];                          
-  $travel_availibility = $row[5]; 
-  $travel_description = $row[6]; 
-  $travel_guide = $row[7]; 
-  $travel_hotel = $row[8]; 
-  $travel_destination = $row[9]; 
-  $image1_url=$row[10];
-  $image2_url=$row[11];
-  $image3_url=$row[12];
-?>
-        <div class="content">
-            <img src="<?php echo $image1_url ?>" class="img-trip">
-            <h3><?php echo htmlspecialchars($travel_destination); ?></h3>
-            <p><?php echo htmlspecialchars($travel_name); ?> </p>
-            <h6><?php echo htmlspecialchars($travel_price); ?> DA</h6>
-            <span class="fa fa-star checked"></span>
-            <span class="fa fa-star checked"></span>
-            <span class="fa fa-star checked"></span>
-            <span class="fa fa-star checked"></span>
-            <span class="fa fa-star checked"></span>
-            <br>
-            <a href="more-info.php?id=<?php echo $travel_id;?>" class="see-more">see more</a>
+            // Pagination configuration
+            $itemsPerPage = 10;
+            $currentPage = isset($_GET['page']) ? $_GET['page'] : 1;
+            $offset = ($currentPage - 1) * $itemsPerPage;
+
+            $query = "SELECT * FROM travel_plan LIMIT $offset, $itemsPerPage";
+            $countQuery = "SELECT COUNT(*) FROM travel_plan";
+
+            if ($result = mysqli_query($conn, $query)) {
+                // Fetch one and one row
+                while ($row = mysqli_fetch_row($result)) {
+                    $travel_id = $row[0];
+                    $travel_name = $row[1];
+                    $travel_date = $row[2];
+                    $travel_price =  $row[3];
+                    $travel_duration = $row[4];
+                    $travel_availibility = $row[5];
+                    $travel_description = $row[6];
+                    $travel_guide = $row[7];
+                    $travel_hotel = $row[8];
+                    $travel_destination = $row[9];
+                    $image1_url = $row[10];
+                    $image2_url = $row[11];
+                    $image3_url = $row[12];
+                    ?>
+                    <div class="content">
+                        <img src="<?php echo $image1_url ?>" class="img-trip">
+                        <h3><?php echo htmlspecialchars($travel_destination); ?></h3>
+                        <p><?php echo htmlspecialchars($travel_name); ?> </p>
+                        <h6><?php echo htmlspecialchars($travel_price); ?> DA</h6>
+                        <span class="fa fa-star checked"></span>
+                        <span class="fa fa-star checked"></span>
+                        <span class="fa fa-star checked"></span>
+                        <span class="fa fa-star checked"></span>
+                        <span class="fa fa-star checked"></span>
+                        <br>
+                        <a href="more-info.php?id=<?php echo $travel_id; ?>" class="see-more">see more</a>
+                    </div>
+                    <?php
+                }//end while
+                // Free result set
+                mysqli_free_result($result);
+
+                
+            }// end if
+            ?>
         </div>
-        <?php
-          }//end while
-               // Free result set
-               mysqli_free_result($result);
-               }// end if
-               
-               mysqli_close($conn);        
-           ?>
-         </div>
-     
+
+        <!-- Pagination -->
+        <div class="pagination-wrapper">
+            <nav aria-label="Page navigation">
+                <ul class="pagination">
+                    <?php
+                    $countResult = mysqli_query($conn, $countQuery);
+                    $rowCount = mysqli_fetch_row($countResult)[0];
+                    $numPages = ceil($rowCount / $itemsPerPage);
+
+                    if ($currentPage > 1) {
+                        echo '<li class="page-item"><a class="page-link" href="?page=1">First</a></li>';
+                        echo '<li class="page-item"><a class="page-link" href="?page=' . ($currentPage - 1) . '">Previous</a></li>';
+                    }
+
+                    for ($i = max(1, $currentPage - 2); $i <= min($currentPage + 2, $numPages); $i++) {
+                        if ($i == $currentPage) {
+                            echo '<li class="page-item active"><a class="page-link" href="?page=' . $i . '">' . $i . '</a></li>';
+                        } else {
+                            echo '<li class="page-item"><a class="page-link" href="?page=' . $i . '">' . $i . '</a></li>';
+                        }
+                    }
+
+                    if ($currentPage < $numPages) {
+                        echo '<li class="page-item"><a class="page-link" href="?page=' . ($currentPage + 1) . '">Next</a></li>';
+                        echo '<li class="page-item"><a class="page-link" href="?page=' . $numPages . '">Last</a></li>';
+                    }
+                    ?>
+                </ul>
+            </nav>
+        </div>
     </section><!-- End Portfolio Section -->
-
-
-   
-  </main><!-- End #main -->
+</main><!-- End #main -->
 
   <!-- ======= Footer ======= -->
   <footer id="footer">
---
+++ trip-reservation.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Reserve a Trip</title>
+    <link href="assets/css/signup.css" rel="stylesheet" type="text/css" >
+    <link href="https://fonts.googleapis.com/css?family=Raleway:400,700" rel="stylesheet">
+    <link href="assets/css/signup.css" rel="stylesheet">
+</head>
+<body>
+    <div class="header">
+        <h1><a href="loggedindex.php">RIHLAT-e</a></h1>
+    </div>
+    <div class="container">
+        <h1> Reserve a Trip</h1>
+        <form method="post">
+            <label>Is the transport included?</label>
+            <label><input type="radio" name="transport" value="yes" checked> Yes</label>
+            <label><input type="radio" name="transport" value="no"> No</label><br>
+            <input type="hidden" name="travel_id" value="<?php echo $_GET['id']; ?>">
+            <button type="submit" name="submit">Reserve Now</button>
+        </form>
+    </div>
+    <?php
+    session_start();
+
+    // Check if the form was submitted
+    if (isset($_POST['submit'])) {
+        // Establish a connection to the database
+        $servername = "localhost";
+        $username = "root";
+        $password = "";
+        $dbname = "rihlate";
+
+        $conn = mysqli_connect($servername, $username, $password, $dbname);
+
+        // Check if the connection was successful
+        if (!$conn) {
+            die("Connection failed: " . mysqli_connect_error());
+        }
+
+        // Retrieve the form data
+        $transport = $_POST['transport'];
+        $travel_id = $_POST['travel_id'];
+        $user_id = $_SESSION['user_id'];
+
+        // Insert the reservation data into the database
+        $sql = "INSERT INTO reservation (user_ID, travel_plan_id, reservation_id, reservation_date, transport)
+                VALUES ('$user_id', '$travel_id', '', CURRENT_DATE, '$transport')";
+
+        if (mysqli_query($conn, $sql)) {
+            // Reservation successful
+            echo "Reservation successful!";
+        } else {
+            echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+        }
+
+        // Close the database connection
+        mysqli_close($conn);
+    }
+    ?>
+</body>
+</html>
